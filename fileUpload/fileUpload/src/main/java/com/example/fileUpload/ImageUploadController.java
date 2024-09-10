5aa2b0f22e2b3a2940267a067996c7b983c2aaf4
Dhanuka Iroshan <109564725+Dhack99@users.noreply.github.com>
2024-09-10 09:42:47 +0530

    initial commit

---
+++ fileUpload/fileUpload/src/main/java/com/example/fileUpload/ImageUploadController.java
@@ -0,0 +1,31 @@
+package com.example.fileUpload;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+
+@RequestMapping("api/v1/img")
+@RestController
+public class ImageUploadController {
+
+    @PostMapping("/upload")
+    public void saveImage(@RequestParam("files")MultipartFile[] files){
+        String uploadDir = "profileImageFolder";
+        Arrays.asList(files).stream().forEach(file->{
+            String fileName= StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+            System.out.println(fileName);
+            try{
+                FileUploadUtill.saveFile(uploadDir,fileName,file);
+            }catch (IOException ioException){
+
+            }
+        });
+    }
+}
